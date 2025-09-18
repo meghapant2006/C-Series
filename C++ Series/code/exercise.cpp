cfd239edb76604cb4aba4f49ba38b6bbc9c5e1b5
Megha pant <pantmegha195@gmail.com>
2025-09-18 22:11:49 +0530

    area

--- C++ Series/code/exercise.cpp
+++ C++ Series/code/exercise.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main()
 {
+    
     int x = 5, y = 6, z = 5;
     cout << x + y + z << "\n";
 
     cout << 3 + 3 << "\n";
     cout << 2 * 5 << "\n";
-
    
+
+    
 }
--- C++ Series/variable/int.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int myAge = 19;
-    cout << "I am " << myAge << " years old." << endl;
-return 0;
-}
