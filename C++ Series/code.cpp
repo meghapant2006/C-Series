17169105cd9e66991a5bfb00600d3b3067d4d10b
Megha pant <pantmegha195@gmail.com>
2025-09-18 21:36:13 +0530

    variable

--- C++ Series/code.cpp
+++ C++ Series/code.cpp
@@ -1,7 +1,16 @@
 
 #include <iostream>
 using namespace std;
+
 int main() {
     cout << "Hello, World!" << endl;
+    cout <<"I am learning C++"<< endl;
     return 0;
 }
+
+// int main() {
+//   cout << 3 + 3 <<"\n";
+//   cout << 2 * 5 <<"\n";
+//   return 0;
+// }
+
---
+++ C++ Series/variable/types.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// 1.int..........integer
+// 2.double.......decimal number
+// 3.char.........character
+// 4.string......sequence of characters
+// 5.bool.........true/false
+
+int main()
+{
+    int myNum = 19;
+    cout << myNum << "\n";
+    
+    double myFloatNum = 45.99;
+    cout << myFloatNum << "\n";
+
+    char myLetter = 'M';
+    cout << myLetter << "\n";
+
+    string myText = "Hello I'm Megha learning C++";
+    cout << myText << "\n";
+
+    bool myBoolean = true;
+    cout << myBoolean << "\n";
+
+    return 0;
+}
