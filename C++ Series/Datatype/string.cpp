bdf243b0193994b6f08ecc0fdccdefba9ee03569
Megha pant <pantmegha195@gmail.com>
2025-09-18 22:24:47 +0530

    datatype

---
+++ C++ Series/Datatype/string.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string myText = "Hello i am learning C++ from wtf coding";    
+    cout << "String: " << myText << "\n";
+    return 0;
+}
---
+++ C++ Series/variable/user_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    int sum;
+    cout << "Type a number: ";
+    cin >> num1;
+    cout << "Type another number: ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Sum is: " << sum;
+}
